b059112cc5d80fe6cb88d69847a04489b2a52b99
gabriel <gabriel.ifpi123@gmail.com>
2026-04-16 09:17:23 -0300

    Coisa nova

---
+++ media3notas.py
@@ -0,0 +1,7 @@
+nota1 = int(input("adicione a primeira nota: "))
+nota2 = int(input("adicione a segunda nota: "))
+nota3 = int(input("adicione a terceira nota: "))
+
+soma = nota1 + nota2 + nota3
+
+print(f"media final:{soma / 3}")
